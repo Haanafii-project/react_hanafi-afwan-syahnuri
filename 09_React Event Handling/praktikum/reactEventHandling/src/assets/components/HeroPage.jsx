07a3fcd3fabcadf689200b97d9944428dd5482eb
Hanafi Afwan Syahnuri <hanafi.afwan.syahnuri@gmail.com>
2024-03-24 23:34:41 +0700

    update tugas react event handling

---
+++ 09_React Event Handling/praktikum/reactEventHandling/src/assets/components/HeroPage.jsx
@@ -0,0 +1,21 @@
+export default function HeroPage () {
+    return (
+        <div>
+            <div className="container mx-auto">
+              <div className="flex justify-center mt-4">
+                <img src="src\assets\images\Tailwind_CSS_Logo.svg.png" alt="" />
+              </div>
+              <div className="flex flex-col items-center">
+                <h1 className="text-3xl py-4">Create Product</h1>
+                <p className="pb-4 text-center">
+                  Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minus quidem
+                  ipsum facere alias itaque animi dicta natus. Eos quasi sed corporis ex
+                  voluptas commodi assumenda maxime maiores ab culpa reiciendis dicta
+                  numquam a molestiae nesciunt placeat necessitatibus cum, ullam delectus ad
+                  praesentium, provident vel! Sed rem nemo consequuntur natus incidunt?
+                </p>
+              </div>
+            </div>
+        </div>
+    )
+}
