a15e9c7d93116c043e59d5938100a5c972877f70
Hanafi Afwan Syahnuri <hanafi.afwan.syahnuri@gmail.com>
2024-05-15 19:11:14 +0700

    update tugas react hook

---
+++ 10_React Hook/praktikum/reactHook/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+import CreateProductPage from "./pages/CreateProductPage"
+
+function App() {
+  return (
+    <>
+      <CreateProductPage/>
+    </>
+  )
+  }
+
+export default App
